9d4e3f6363ac9a233f8faed6c8b625de07746ca4
vksychev <vksychev@yandex.ru>
2018-04-12 07:18:31 +0300

    filters changed

--- library/iscsi_package/iscsi_sample_create.py
+++ library/iscsi_package/iscsi_sample_create.py
@@ -1,6 +1,6 @@
 from update import runUpdate
 def create_iscsi_potal(params):
-    ethPort = params['ethPort']
+    ethPort = params['ethernetPort']
     ipAddress = params['ipAddress']
     netmask = params['netmask']
     update = {'resource_type': 'iscsiPortal', 'ethernetPort': ethPort, 'ipAddress': ipAddress,"netmask": netmask}
