2fa54b7c0f11c69278143d68fbf3f59b98bca555
vksychev <vksychev@yandex.ru>
2018-04-12 07:28:01 +0300

    update delete

--- library/iscsi_package/iscsi_sample_create.py
+++ library/iscsi_package/iscsi_sample_create.py
@@ -1,8 +1,9 @@
 from update import runUpdate
+from dellemc_unity import Unity
 def create_iscsi_potal(params):
     ethPort = params['ethernetPort']
     ipAddress = params['ipAddress']
     netmask = params['netmask']
     update = {'resource_type': 'iscsiPortal', 'ethernetPort': ethPort, 'ipAddress': ipAddress,"netmask": netmask}
-    runUpdate(update)
+    Unity.runUpdate(update)
 
