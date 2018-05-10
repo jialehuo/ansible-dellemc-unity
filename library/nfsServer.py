2736ef36972a21b00e894495081cd85473cb8af1
Alexandr Aphonin <alex15051997@yandex.ru>
2018-05-10 16:40:02 +0300

    add nasServer, nfsServer,fileInterface modules and playbooks not add optional_list for fileInterface

--- library/nfsServer.py
+++ library/nfsServer.py
@@ -4,9 +4,8 @@ from dellemc_unity_sdk import validator
 from dellemc_unity_sdk.unity import Unity
 from dellemc_unity_sdk import runner
 
-optional_list = {'tenant', 'isReplicationDestination', 'currentUnixDirectoryService', 
-    'isMultiProtocolEnabled','allowUnmappedUser','defaultUnixUser','defaultWindowsUser',
-    'isPacketReflectEnabled'}
+optional_list = {'hostName','nfsv4Enabled','isSecureEnabled','kdcType','kdcUsername','kdcPassword',
+'isExtendedCredentialsEnabled','credentialsCacheTTL'}
 
 def _exception_about_parameters(supported_parameters):
     return False, 'You did not input required parameters or inputted unsupported parameter, ' \
@@ -16,23 +15,20 @@ def _exception_about_parameters(supported_parameters):
 #a dict with req and opt params
 def create(params, unity):
     
-    required_list = {'name','hostSP','pool'}
+    required_list = {'nasServer'}
     all_params = {'required': required_list,
                   'optional': optional_list}
     # TODO: how to output errors or some messages
-    name = params['name']
-    hostSP = params['hostSP']
-    pool = params['pool']
+    nasServer = params['nasServer']
+
     if not validator.check_parameters(params, all_params):
             return _exception_about_parameters(all_params)
-    request_params = {'name': name, 'hostSP': hostSP, 'pool': pool}
+    request_params = {'nasServer': nasServer}
             for parameter in optional_list:
             if params.get(parameter):
                 request_params.update({parameter: params.get(parameter)})
-    reply = unity.update('create', 'nasServer', request_params)
-    return True, {'nasServer': {'id': reply['entries'][0]['id']}}
-
-
+    reply = unity.update('create', 'nfsServer', request_params)
+    return True, {'nfsServer': {'id': reply['entries'][0]['id']}}
 
 def delete(params, unity):
     all_params = {'required':'id'}
@@ -40,20 +36,18 @@ def delete(params, unity):
             return _exception_about_parameters(all_params)
     if 'id' not in params:
         return False, 'you must input id'
-    nasServer_id = params['id']
-    unity.update('delete', 'nasServer', {'id': nasServer_id})
+    nfsServer_id = params['id']
+    unity.update('delete', 'nfsServer', {'id': nfsServer_id})
     return True, ''
 
 def modify(params, unity):
     required_list = {'id'}
-    optional_list.add('name') #TODO: we must not change global opt_list. figure out a workaround
-    optional_list.add('homeSP')
-    optional_list.add('pool')
-        
+    optional_list.add('nfsServer') #TODO: we must not change global opt_list. figure out a workaround
+
         all_params = {'required': required_list, 'optional': optional_list}
         if not validator.check_parameters(params, all_params):
             return _exception_about_parameters(all_params)
-        unity.update('modify', 'nasServer', params)
+        unity.update('modify', 'nfsServer', params)
         return True, ''
 
 
