be4c7e26c88fadc54ec4ff07c9dd72534a740ae1
Andrew Petrov (marsofandrew) <marsofandrew@gmail.com>
2018-07-03 14:27:28 +0300

    add file system

---
+++ library/system.py
@@ -0,0 +1,9 @@
+from ansible.module_utils.basic import AnsibleModule
+from dellemc_unity_sdk import runner
+from dellemc_unity_sdk import constants
+
+ANSIBLE_METADATA = {'metadata_version': '0.1',
+                    'status': ['unstable'],
+                    'supported_by': 'community'}
+
+
