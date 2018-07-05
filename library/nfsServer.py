ab7de1e7b0bf6b6f8e10df8494edf2109de4b9ef
Alexandr Aphonin <alex15051997@yandex.ru>
2018-07-05 14:32:37 +0300

    Add nfsServer and fileInterface modules for SDK 0.4
    Add modules for nfsServer and fileinterface for SDK 0.4
    Correct indents for nasServer.py

--- library/nfsServer.py
+++ library/nfsServer.py
@@ -1,75 +1,55 @@
 #!/usr/bin/python
-ANSIBLE_METADATA = {'metadata_version': '0.1',
-                    'status': ['unstableinterface'],
-                    'supported_by': 'students'}
+
 from ansible.module_utils.basic import AnsibleModule
-from dellemc_unity_sdk import validator
-from dellemc_unity_sdk.unity import Unity
 from dellemc_unity_sdk import runner
-import json
-optional_list = {'hostName','nfsv4Enabled','isSecureEnabled','kdcType','kdcUsername','kdcPassword',
-'isExtendedCredentialsEnabled','credentialsCacheTTL'}
+from dellemc_unity_sdk import validator
+from dellemc_unity_sdk import supportive_functions
+from dellemc_unity_sdk.unity import Unity
+from dellemc_unity_sdk import constants
 
-def _exception_about_parameters(supported_parameters):
-    return False, 'You did not input required parameters or inputted unsupported parameter, ' \
-                  'supported parameters = ' + supported_parameters.__str__()
+ANSIBLE_METADATA = {'metadata_version': '0.1',
+                    'status': ['unstable'],
+                    'supported_by': 'community'}
 
+parameters_all = {
+    'create': {
+        'required': {'nasServer'},
+        'optional': {'hostName','nfsv4Enabled','isSecureEnabled','kdcType','kdcUsername','kdcPassword',
+                    'isExtendedCredentialsEnabled','credentialsCacheTTL'},
+        },
+    'modify': {
+        'required': {'id'},
+        'optional': {'nasServer','hostName','nfsv4Enabled','isSecureEnabled','kdcType','kdcUsername','kdcPassword',
+                    'isExtendedCredentialsEnabled','credentialsCacheTTL'},
+        },
+    'delete': {'required': {'id'}}
+}
 
-def _get_message_from_update(resp_from_request):
-  result = {}
-  if resp_from_request and resp_from_request.text:
-    dictionary = json.loads(resp_from_request.text)
-    result=dictionary.get('content')
-  return result
-
-#a dict with req and opt params
-def create(params, unity):
-    
-    required_list = {'nasServer'}
-    all_params = {'required': required_list,
-                  'optional': optional_list}
-    # TODO: how to output errors or some messages
-    nasServer = params.get('nasServer')
-
-    if not validator.check_parameters(params, all_params):
-            return _exception_about_parameters(all_params)
-    request_params = {'nasServer': nasServer}
-    for parameter in optional_list:
-        if params.get(parameter):
-            request_params.update({parameter: params.get(parameter)})
-    reply = unity.update('create', 'nfsServer', request_params)
-    cont = _get_message_from_update(reply)
-    return True, cont
-    #id
-    #reply
-    #{'nfsServer': {'id': reply['entries'][0]['id']}}
-
-def delete(params, unity):
-    all_params = {'required': {'id'}}
-    repl = unity.query('nasServer', {'fields': 'id'})
-    if not validator.check_parameters(params, all_params):
-            return _exception_about_parameters(all_params)
-    if 'id' not in params:
-        return False, 'you must input id'
-    nfsServer_id = params['id']
-    reply = unity.update('delete', 'nfsServer', {'id': nfsServer_id})
-    return True, ''
-    #reply
-
-def modify(params, unity):
-    required_list = {'id'}
-    optional_list.add('nfsServer') #TODO: we must not change global opt_list. figure out a workaround
-
-    all_params = {'required': required_list, 'optional': optional_list}
-    if not validator.check_parameters(params, all_params):
-        return _exception_about_parameters(all_params)
-    unity.update('modify', 'nfsServer', params)
-    return True, ''
+template = {
+    constants.REST_OBJECT_KEY: 'nfsServer',
+    constants.ACTIONS_KEY: {
+        'create': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('create')},
+        'modify': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('modify')},
+        'delete': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('delete')}
+    }
+}
 
 
 def main():
-    runner.run([{"function":create},{"function":delete},{"function":modify}])
+    arguments = runner.create_arguments_for_ansible_module([
+        {constants.ACTION_NAME: 'create'},
+        {constants.ACTION_NAME: 'modify'},
+        {constants.ACTION_NAME: 'delete'}])
+
+    ansible_module = AnsibleModule(arguments, supports_check_mode=True)
+    runner.run(ansible_module, template)
 
 
 if __name__ == '__main__':
-    main()
+main()
