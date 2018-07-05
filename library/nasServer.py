cfbcbbce2271315cd0abe5edd68f1c57b08a5398
Alexandr Aphonin <alex15051997@yandex.ru>
2018-07-05 14:17:55 +0300

    Add nasServer module for SDK 0.4
    Add module for work with nasServer for version 0.4

--- library/nasServer.py
+++ library/nasServer.py
@@ -1,69 +1,55 @@
 #!/usr/bin/python
-ANSIBLE_METADATA = {'metadata_version': '0.1',
-                    'status': ['unstableinterface'],
-                    'supported_by': 'students'}
+
 from ansible.module_utils.basic import AnsibleModule
-from dellemc_unity_sdk import validator
-from dellemc_unity_sdk.unity import Unity
 from dellemc_unity_sdk import runner
+from dellemc_unity_sdk import validator
+from dellemc_unity_sdk import supportive_functions
+from dellemc_unity_sdk.unity import Unity
+from dellemc_unity_sdk import constants
 
-optional_list = {'tenant', 'isReplicationDestination', 'currentUnixDirectoryService', 
-    'isMultiProtocolEnabled','allowUnmappedUser','defaultUnixUser','defaultWindowsUser',
-    'isPacketReflectEnabled'}
+ANSIBLE_METADATA = {'metadata_version': '0.1',
+                    'status': ['unstable'],
+                    'supported_by': 'community'}
 
-def _exception_about_parameters(supported_parameters):
-    return False, 'You did not input required parameters or inputted unsupported parameter, ' \
-                  'supported parameters = ' + supported_parameters.__str__()
+parameters_all = {
+    'create': {
+        'required': {'name','homeSP','pool'},
+        'optional': {'tenant', 'isReplicationDestination', 'currentUnixDirectoryService', 'isMultiProtocolEnabled',
+        'allowUnmappedUser','defaultUnixUser','defaultWindowsUser','isPacketReflectEnabled'},
+        },
+    'modify': {
+        'required': {'id'},
+        'optional': {'name','homeSP','pool','tenant', 'isReplicationDestination', 'currentUnixDirectoryService', 
+        'isMultiProtocolEnabled','allowUnmappedUser','defaultUnixUser','defaultWindowsUser','isPacketReflectEnabled'},
+        },
+    'delete': {'required': {'id'}}
+}
 
-
-#a dict with req and opt params
-def create(params, unity):
-    
-    required_list = {'name','homeSP','pool'}
-    all_params = {'required': required_list,
-                  'optional': optional_list}
-    # TODO: how to output errors or some messages
-    name = params['name']
-    homeSP = params['homeSP']
-    pool = params['pool']
-    if not validator.check_parameters(params, all_params):
-            return _exception_about_parameters(all_params)
-    request_params = {'name': name, 'homeSP': homeSP, 'pool': pool}
-    for parameter in optional_list:
-        if params.get(parameter):
-            request_params.update({parameter: params.get(parameter)})
-    reply = unity.update('create', 'nasServer', request_params)
-    return True, ""
-    #{'nasServer': {'id': reply['entries'][0]['id']}}
-
-
-
-def delete(params, unity):
-    all_params = {'required':'id'}
-    if not validator.check_parameters(params, all_params):
-            return _exception_about_parameters(all_params)
-    if 'id' not in params:
-        return False, 'you must input id'
-    nasServer_id = params['id']
-    unity.update('delete', 'nasServer', {'id': nasServer_id})
-    return True, ''
-
-def modify(params, unity):
-    required_list = {'id'}
-    optional_list.add('name') #TODO: we must not change global opt_list. figure out a workaround
-    optional_list.add('homeSP')
-    optional_list.add('pool')
-        
-    all_params = {'required': required_list, 'optional': optional_list}
-    if not validator.check_parameters(params, all_params):
-        return _exception_about_parameters(all_params)
-    unity.update('modify', 'nasServer', params)
-    return True, ''
+template = {
+    constants.REST_OBJECT_KEY: 'nasServer',
+    constants.ACTIONS_KEY: {
+        'create': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('create')},
+        'modifyLun': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('modify')},
+        'delete': 
+        {constants.ACTION_TYPE_KEY:constants.ActionType.UPDATE, 
+            constants.PARAMETER_TYPES_KEY:parameters_all.get('delete')}
+    }
+}
 
 
 def main():
-    runner.run([{"function":create},{"function":delete},{"function":modify}])
+    arguments = runner.create_arguments_for_ansible_module([
+        {constants.ACTION_NAME: 'create'},
+        {constants.ACTION_NAME: 'modify'},
+        {constants.ACTION_NAME: 'delete'}])
+
+    ansible_module = AnsibleModule(arguments, supports_check_mode=True)
+    runner.run(ansible_module, template)
 
 
 if __name__ == '__main__':
-    main()
+main()
